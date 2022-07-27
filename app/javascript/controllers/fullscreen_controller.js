cdf29b39c58c176be52c950e9a518106c372ed26
Jacques Antoine de Cugnac <ja.decugnac@outlook.com>
2022-07-27 15:15:59 +0200

    added a fullscreen display for the pictures, modified the size of show's carousel

--- app/javascript/controllers/fullscreen_controller.js
+++ app/javascript/controllers/fullscreen_controller.js
@@ -1,16 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
+let test = "test";
+
 export default class extends Controller {
 
-  static targets = [ "key", "top" ]
+  static targets = [ "key0", "key1", "key2", "key3", "key4", "key5", "key6", "key7", "key8", "key9", "key10", "key11", "key12", "img", "number" ]
 
   connect() {
-    console.log("Hello there!")
+    console.log("lala");
+    document.getElementById("img0").click();
   }
 
+
   setBackground() {
-    var key = "\"background-image: <%= cl_image_tag(\"" + this.keyTarget.innerText + "\")\""
-    console.log(key)
-    this.topTarget.setAttribute("style", key)
+    const images = document.querySelectorAll("img").forEach((img) => {
+      img.addEventListener("click", (event) => {
+        event.preventDefault();
+        let url = event.currentTarget.className;
+        this.imgTarget.setAttribute("src", url);
+      })
+    });
   }
 }
