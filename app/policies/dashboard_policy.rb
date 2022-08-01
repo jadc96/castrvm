53b29708b8b4fa85dfcaee7fe26f802c8abc2ecf
Jacques Antoine de Cugnac <ja.decugnac@outlook.com>
2022-08-01 10:39:15 +0200

    superclass mismatch fixed

--- app/policies/dashboard_policy.rb
+++ app/policies/dashboard_policy.rb
@@ -1,4 +1,4 @@
-class BookingPolicy < ApplicationPolicy
+class DashboardPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
